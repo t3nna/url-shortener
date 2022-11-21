3770cd7ccddf21772d871581a7ee0c94a8a5dd96
t3nna <t3nna1001@gmail.com>
2022-11-21 09:32:51 +0100

    Monitor process
    Memory, Cpu

---
+++ utils/monitorProcess.js
@@ -0,0 +1,26 @@
+const {exec: execOnCallbacks} = require("child_process")
+const util = require("util")
+const fs = require("fs/promises")
+const path = require("path")
+
+
+const exec = util.promisify(execOnCallbacks)
+
+async function monitorProcess(){
+    setInterval(async ()=>{
+        // id of the process
+        const {pid} = process
+
+        const {stdout} = await exec(
+            `ps -p ${pid} -o pid,vsz=MEMORY,pcpu -o comm,args=ARGS`
+        )
+        const log = `${new Date()}\n${stdout}`
+
+        await fs.writeFile(path.resolve(__dirname, "../cpu-memory.log"), log,{
+            flag: 'a'
+        })
+    }, 5000)
+
+}
+
+module.exports = {monitorProcess}
